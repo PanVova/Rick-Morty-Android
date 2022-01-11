f0d30e01e92146befe92cb2be9f6b510e3d2f707
samurai <panvovandrik@gmail.com>
2022-01-11 17:59:00 +0200

    divide packages

--- app/src/main/java/com/panvova/rickmorty/presentation/MainActivity.kt
+++ app/src/main/java/com/panvova/rickmorty/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.panvova.rickmorty
+package com.panvova.rickmorty.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.panvova.rickmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
