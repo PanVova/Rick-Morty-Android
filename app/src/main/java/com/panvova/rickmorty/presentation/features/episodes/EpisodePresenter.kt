edf26a05952ccf84f3f29ba7db0040d65f17a515
samurai <panvovandrik@gmail.com>
2022-01-12 17:06:36 +0200

    refactor. move some functions to base class

--- app/src/main/java/com/panvova/rickmorty/presentation/features/episodes/EpisodePresenter.kt
+++ app/src/main/java/com/panvova/rickmorty/presentation/features/episodes/EpisodePresenter.kt
@@ -3,28 +3,18 @@ package com.panvova.rickmorty.presentation.features.episodes
 import com.panvova.rickmorty.domain.usecase.GetEpisodesUseCase
 import com.panvova.rickmorty.domain.viewstate.EpisodeViewState
 import com.panvova.rickmorty.presentation.base.BasePresenter
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
-import javax.inject.Named
 
 class EpisodePresenter @Inject constructor(
     private val episodesUseCase: GetEpisodesUseCase,
-    @Named("Main") private val coroutineDispatcherMain: CoroutineDispatcher,
-    @Named("IO") private val coroutineDispatcherIO: CoroutineDispatcher
 ) : BasePresenter<EpisodeView>() {
 
-
-    override fun attachView(view: EpisodeView) {
-        super.attachView(view)
-        loadEpisodes()
-    }
-
-    private fun loadEpisodes() {
+    override fun loadData() {
         CoroutineScope(coroutineDispatcherIO).launch {
             view?.render(EpisodeViewState.Loading)
             getEpisodes()
--- app/src/main/java/com/panvova/rickmorty/presentation/features/locations/LocationPresenter.kt
+++ app/src/main/java/com/panvova/rickmorty/presentation/features/locations/LocationPresenter.kt
@@ -3,27 +3,18 @@ package com.panvova.rickmorty.presentation.features.locations
 import com.panvova.rickmorty.domain.usecase.GetLocationsUseCase
 import com.panvova.rickmorty.domain.viewstate.LocationViewState
 import com.panvova.rickmorty.presentation.base.BasePresenter
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
-import javax.inject.Named
 
 class LocationPresenter @Inject constructor(
     private val locationsUseCase: GetLocationsUseCase,
-    @Named("Main") private val coroutineDispatcherMain: CoroutineDispatcher,
-    @Named("IO") private val coroutineDispatcherIO: CoroutineDispatcher,
 ) : BasePresenter<LocationView>() {
 
-    override fun attachView(view: LocationView) {
-        super.attachView(view)
-        loadLocations()
-    }
-
-    private fun loadLocations() {
+    override fun loadData() {
         CoroutineScope(coroutineDispatcherIO).launch {
             view?.render(LocationViewState.Loading)
             getLocations()
